ce9e07bfe365e22992be5b3208ac4009564c4002
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-14 22:07:35 +0100

    Part 1 of the tutorial solved: https://bost.ocks.org/mike/bar/

--- src/ole.js
+++ src/ole.js
@@ -7,13 +7,21 @@ var ole;
         chart.prototype.init = function (container) {
             this._container = d3.select(container).selectAll("div");
         };
+        // The explanation for this methods can be found here:
+        // https://bost.ocks.org/mike/bar/
         chart.prototype.render = function (data) {
+            // Determine the X scale
+            var maxValue = d3.max(data);
+            var scaleLinear = d3.scaleLinear()
+                .domain([0, maxValue])
+                .range([0, 420]);
+            // Draw the bars
             this._container
                 .data(data)
                 .enter()
                 .append("div")
                 .style("width", function (d) {
-                return d + "px";
+                return scaleLinear(d) + "px";
             })
                 .text(function (s) {
                 return s;
--- src/ole.ts
+++ src/ole.ts
@@ -10,13 +10,23 @@ module ole {
             this._container = d3.select(container).selectAll("div");
         }
 
+        // The explanation for this methods can be found here:
+        // https://bost.ocks.org/mike/bar/
         public render(data) {
+
+            // Determine the X scale
+            let maxValue = d3.max(data as ArrayLike<number>);
+            let scaleLinear = d3.scaleLinear()
+                .domain([0, maxValue])
+                .range([0, 420]);
+
+            // Draw the bars
             this._container
                 .data(data)
                 .enter()
                 .append("div")
-                .style("width", function (d) {
-                    return d + "px";
+                .style("width", function (d: number): string {
+                    return scaleLinear(d) + "px";
                 })
                 .text(function (s: string): string {
                     return s
