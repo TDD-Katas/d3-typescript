f7a4bb5e8619f3513c3518167b755fec379a94fb
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-14 23:18:27 +0100

    Part 3 of the tutorial solved: https://bost.ocks.org/mike/bar/

--- src/ole.ts
+++ src/ole.ts
@@ -1,61 +1,54 @@
-
 module ole {
     export class chart {
-        private _container: d3.Selection<any, any, any, any>;
-
-        constructor(container: any) {
-            this.init(container);
-        }
-
-        private init(container) {
-            this._container = d3.select(container);
-        }
 
         // The explanation for this methods can be found here:
         // https://bost.ocks.org/mike/bar/
-        public render(dataLocation, width: number, barHeight: number) {
+        public render(dataLocation) {
 
-            // 1. Code here runs first, before the download starts.
-            let scaleLinear = d3.scaleLinear()
-                .range([0, 420]);
+            let svg = d3.select("svg");
+            let margin = {top: 20, right: 20, bottom: 30, left: 40};
+            let width = +svg.attr("width") - margin.left - margin.right;
+            let height = +svg.attr("height") - margin.top - margin.bottom;
 
-            let chart = this._container
-                .attr("width", width);
+            let x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+            let y = d3.scaleLinear().rangeRound([height, 0]);
+
+            var g = svg.append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
             d3.tsv(dataLocation, function (d: any) {
-                d.value = +d.value;
+                d.frequency = +d.frequency;
                 console.log(d);
                 return d;
             }, function (error, data) {
-                // 3. Code here runs last, after the download finishes.
-                scaleLinear.domain([0, d3.max(data, function (d: any) {
-                    return d.value;
-                })]);
+                if (error) throw error;
 
-                chart.attr("height", barHeight * data.length);
+                x.domain(data.map(function(d) { return d.letter; }));
+                y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-                let bar = chart.selectAll("g")
+                g.append("g")
+                    .attr("class", "axis axis--x")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(d3.axisBottom(x));
+
+                g.append("g")
+                    .attr("class", "axis axis--y")
+                    .call(d3.axisLeft(y).ticks(10, "%"))
+                    .append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", "0.71em")
+                    .attr("text-anchor", "end")
+                    .text("Frequency");
+
+                g.selectAll(".bar")
                     .data(data)
-                    .enter().append("g")
-                    .attr("transform", function (d: any, i: number) {
-                        return "translate(0," + i * barHeight + ")";
-                    });
-
-                bar.append("rect")
-                    .attr("width", function (d: any) {
-                        return scaleLinear(d.value);
-                    })
-                    .attr("height", barHeight - 1);
-
-                bar.append("text")
-                    .attr("x", function (d: any) {
-                        return scaleLinear(d.value) - 3;
-                    })
-                    .attr("y", barHeight / 2)
-                    .attr("dy", ".35em")
-                    .text(function (d) {
-                        return d.value;
-                    });
+                    .enter().append("rect")
+                    .attr("class", "bar")
+                    .attr("x", function(d) { return x(d.letter); })
+                    .attr("y", function(d) { return y(d.frequency); })
+                    .attr("width", x.bandwidth())
+                    .attr("height", function(d) { return height - y(d.frequency); });
             });
 
             // 2. Code here runs second, while the file is downloading.
